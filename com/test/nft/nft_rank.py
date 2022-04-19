e46de5ab5cbdd829bb01bf620ae8a28d564dfffa
NIO <hugenshen163@163.com>
2022-04-20 01:37:27 +0800

    fix:

--- com/test/nft/nft_rank.py
+++ com/test/nft/nft_rank.py
@@ -14,18 +14,22 @@ def jaccardDistance(NFT1, NFT2):
 """
 稀有度 
 """
-def rareRank(tar_NFT, all_NFT):
-    jd_results = []
-    # 计算所有的JD
-    for each_NFT in all_NFT:
-        jd_results.append(jaccardDistance(tar_NFT, each_NFT))
-    # 平均值作为当前NFT的初始稀有度,放在集合最后一个
-    jd_results.append(np.mean(jd_results))
-    # z-score归一化
-    jd_results = np.array(jd_results)
-    jd_zscore = (jd_results - jd_results.min()) / (jd_results.max() - jd_results.min())
+def rareRank(all_NFT):
 
+    jd_means=[]
+    #计算所有的JD
+    for i,each_NFT in enumerate(all_NFT):
+        jd_results = []
+        for other_NFT in all_NFT[0:i]+all_NFT[i+1:]:
+            jd_results.append(jaccardDistance(each_NFT,other_NFT))
+        jd_means.append(np.mean(jd_results))
+
+    #平均值作为当前NFT的初始稀有度,放在集合最后一个
+    jd_means=np.array(jd_means)
+    #z-score归一化
+    jd_zscore=(jd_means-jd_means.min())/(jd_means.max()-jd_means.min())
     return jd_zscore
+
 
 """
 main方法
